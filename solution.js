e93c3fe0ede7cf73553374db6fd783dd331c304c
benjamin DURON <benjaminduron@MacBook-Pro.local>
2020-11-29 20:57:12 +0100

    sol

---
+++ solution.js
@@ -0,0 +1,106 @@
+import { capitalize, downTo } from './helpers';
+
+class BottleVerse {
+  static lyrics(number) {
+    return new BottleVerse(BottleNumber.for(number)).lyrics();
+  }
+
+  constructor(bottleNumber) {
+    this.bottleNumber = bottleNumber;
+  }
+
+  lyrics() {
+    return (
+      capitalize(`${this.bottleNumber} of beer on the wall, `) +
+      `${this.bottleNumber} of beer.\n` +
+      `${this.bottleNumber.action()}, ` +
+      `${this.bottleNumber.successor()} of beer on the wall.\n`
+    );
+  }
+}
+
+class BottleNumber {
+  static for(number) {
+    let bottleNumberClass;
+    switch (number) {
+      case 0:
+        bottleNumberClass = BottleNumber0;
+        break;
+      case 1:
+        bottleNumberClass = BottleNumber1;
+        break;
+      case 6:
+        bottleNumberClass = BottleNumber6;
+        break;
+      default:
+        bottleNumberClass = BottleNumber;
+        break;
+    }
+
+    return new bottleNumberClass(number);
+  }
+
+  constructor(number) {
+    this.number = number;
+  }
+
+  toString() {
+    return `${this.quantity()} ${this.container()}`;
+  }
+
+  quantity() {
+    return this.number.toString();
+  }
+
+  container() {
+    return 'bottles';
+  }
+
+  action() {
+    return `Take ${this.pronoun()} down and pass it around`;
+  }
+
+  pronoun() {
+    return 'one';
+  }
+
+  successor() {
+    return BottleNumber.for(this.number - 1);
+  }
+}
+
+class BottleNumber0 extends BottleNumber {
+  quantity() {
+    return 'no more';
+  }
+
+  action() {
+    return 'Go to the store and buy some more';
+  }
+
+  successor() {
+    return BottleNumber.for(99);
+  }
+}
+
+class BottleNumber1 extends BottleNumber {
+  container() {
+    return 'bottle';
+  }
+
+  pronoun() {
+    return 'it';
+  }
+}
+
+class BottleNumber6 extends BottleNumber {
+  quantity() {
+    return '1';
+  }
+
+  container() {
+    return 'six-pack';
+  }
+}
+
+export { BottleVerse, CountdownSong };
