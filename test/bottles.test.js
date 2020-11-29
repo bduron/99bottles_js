186331647341b9244fe99b34f0bae93f67c3b95a
Augustin BARBE <augustin.barbe@gmail.com>
2020-11-29 20:01:37 +0100

    add case

--- test/bottles.test.js
+++ test/bottles.test.js
@@ -59,6 +59,24 @@ describe('BottleVerse', () => {
       '98 bottles of beer on the wall.\n';
     expect(BottleVerse.lyrics(99)).toBe(expected);
   });
+  
+  test('verse 7', () => {
+    const expected =
+      '7 bottles of beer on the wall, ' +
+      '7 bottles of beer.\n' +
+      'Take one down and pass it around, ' +
+      '1 six-pack of beer on the wall.\n';
+    expect(BottleVerse.lyrics(7)).toBe(expected);
+  });
+
+  test('verse 6', () => {
+    const expected =
+      '1 six-pack of beer on the wall, ' +
+      '1 six-pack of beer.\n' +
+      'Take one down and pass it around, ' +
+      '5 bottles of beer on the wall.\n';
+    expect(BottleVerse.lyrics(6)).toBe(expected);
+  });
 
   test('verse general rule lower bound', () => {
     const expected =
